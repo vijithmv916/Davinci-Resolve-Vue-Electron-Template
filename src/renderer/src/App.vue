f9712f7736611a6762db15de43963ad668c1194e
vijith mv <vijith@emamsolutions.com>
2023-09-14 17:47:09 +0530

    chore: unocss

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <div class="w10 h10 bg-yellow-100"></div>
+    <h1 font-sans>new</h1>
+  </div>
+</template>
+
+<!-- <style lang="less">
+@import './assets/css/styles.less';
+</style> -->
